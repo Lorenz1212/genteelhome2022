035cb72c7568d865a9c8b23836f5dffb614c845c
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-04-01 10:16:02 +0800

    Update footer.php

--- application/views/website/layouts/footer.php
+++ application/views/website/layouts/footer.php
@@ -71,11 +71,11 @@
                         </div>
                         <div class="col-sm-6 links">
                             <ul>
-                                <li><a href="https://www.facebook.com/GenteelHomePH"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="https://www.facebook.com/GenteelHomePH" target="_blank"><i class="fa fa-facebook"></i></a></li>
 <!--                                 <li><a href="#"><i class="fa fa-twitter"></i></a></li> -->
-                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                <li><a href="finance@genteelhome.com"  target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                                <li><a href="#"  target="_blank"><i class="fa fa-youtube"></i></a></li>
+                                <li><a href="https://www.instagram.com/home_genteel/"  target="_blank"><i class="fa fa-instagram"></i></a></li>
                             </ul>
                         </div>
                     </div>
