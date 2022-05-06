4732aef18b27a714ed45d66cb4dad9ef0fa9e989
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-05-06 16:15:14 +0800

    Update form-controls.js

--- assets/js/pages/crud/forms/validation/form-controls.js
+++ assets/js/pages/crud/forms/validation/form-controls.js
@@ -45,13 +45,13 @@ var KTFormControls = function () {
 	var _initnotificationupdate = function(){
 		 let url = window.location.pathname;
 		 let urlpost;
-		 if(url.split('/')[0] == 'genteelhome2022'){
+		 if(url.split('/')[0] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[2];
-		 }else if(url.split('/')[1] == 'genteelhome2022'){
+		 }else if(url.split('/')[1] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[3];
-		 }else if(url.split('/')[2] == 'genteelhome2022'){
+		 }else if(url.split('/')[2] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[4];
-		 }else if(url.split('/')[3] == 'genteelhome2022'){
+		 }else if(url.split('/')[3] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[6];
 		 }
 		 if(urlpost == 'designer'){
