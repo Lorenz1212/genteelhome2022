5fe50a6c9449a087e96ac7163fbff80d101d6e72
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-05-06 16:26:30 +0800

    Update accounting-form-controls.js

--- assets/js/pages/crud/forms/validation/accounting-form-controls.js
+++ assets/js/pages/crud/forms/validation/accounting-form-controls.js
@@ -128,13 +128,13 @@ var KTFormControls = function () {
 	var _initnotificationupdate = function(){
 		 let url = window.location.pathname;
 		 let urlpost;
-		 if(url.split('/')[0] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
+		  if(url.split('/')[0] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[2];
-		 }else if(url.split('/')[1] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
+		 }else if(url.split('/')[1] == 'genteelhome2022' || url.split('/')[1] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[3];
-		 }else if(url.split('/')[2] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
+		 }else if(url.split('/')[2] == 'genteelhome2022' || url.split('/')[2] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[4];
-		 }else if(url.split('/')[3] == 'genteelhome2022' || url.split('/')[0] == 'genteelhomev2'){
+		 }else if(url.split('/')[3] == 'genteelhome2022' || url.split('/')[3] == 'genteelhomev2'){
 		 	urlpost = url.split('/')[6];
 		 }
 		 if(urlpost == 'designer'){
