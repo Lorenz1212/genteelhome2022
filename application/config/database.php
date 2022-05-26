d64cf670eab9f2e6b74a2958115b7b438fb3cf87
Lorenz-lyb <87624151+lyb-011@users.noreply.github.com>
2022-05-26 16:25:09 +0800

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -74,34 +74,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 //localhost
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'genteelhomedemo',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt'  => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-
 // $db['default'] = array(
 // 	'dsn'	=> '',
-// 	'hostname' => 'p3plzcpnl485018.prod.phx3.secureserver.net',
-// 	'username' => 'lorenz1212',
-// 	'password' => '@Wdqse1212',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
 // 	'database' => 'genteelhomedemo',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
@@ -112,9 +89,32 @@ $db['default'] = array(
 // 	'char_set' => 'utf8',
 // 	'dbcollat' => 'utf8_general_ci',
 // 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
+// 	'encrypt'  => FALSE,
 // 	'compress' => FALSE,
 // 	'stricton' => FALSE,
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'p3plzcpnl485018.prod.phx3.secureserver.net',
+	'username' => 'lorenz1212',
+	'password' => '@Wdqse1212',
+	'database' => 'genteelhomedemo',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
