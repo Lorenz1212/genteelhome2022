17bdaa350273b233ae130c947da7f6e895c77fa0
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-06-15 23:36:14 +0800

    update

--- application/views/reviewer/tutorial.php
+++ application/views/reviewer/tutorial.php
@@ -119,8 +119,150 @@
 				 </div>
 				</div>
 		 	</div>
+		 	<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     REQUEST - Purchase Request For Stocks
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-purchase-stocks.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
 
+		 		<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     REQUEST - Material Request For Stocks
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-material-stocks-request.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
 
+		 		<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     REQUEST - Request Material For Office/Janitorial Supplies
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-request-office.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
+
+		 	<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     Joborder Masterlist
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-joborder-stocks.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
+		 	<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     Delivery Receipt
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-dr.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
+		 	
+		 		<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     Return item to warehouse
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-return-item.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
+		 		<div class="col-4">
+		 		<div class="card card-custom gutter-b">
+				 <div class="card-header">
+				  <div class="card-title">
+				   <h3 class="card-label">
+				     Return item from customer
+				   </h3>
+				  </div>
+				 </div>
+				 <div class="card-body">
+				 	<div class="row">
+				 		<div class="col">
+				 			 <video width="100%" height="100%" controls>
+							  <source src="<?php echo base_url('assets/video_tutorial/inventory-return-customer.mp4') ?>" type="video/mp4">
+							</video>
+				 		</div>
+				 	</div>
+				 </div>
+				</div>
+		 	</div>
 
 		 </div>
 	</div>
