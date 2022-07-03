493b51fb2379f462affb0473dd37bcfec27f176a
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-07-03 18:19:18 +0800

    update

--- application/views/website/layouts/header.php
+++ application/views/website/layouts/header.php
@@ -11,8 +11,30 @@
     <meta name="description" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
     <meta name="author" content="Genteel Home">
 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+     <title>Official Genteel Home - Furniture + Boutique + Lifestyle</title>
+
+    <meta property="og:title" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
+    <meta property="og:site_name" content="TotooBa.info">
+    <meta property="og:url" content="https://totooba.info/">
+    <meta property="og:description" content="TotooBa.Info is a disinformation reporting platform developed under the Initiative for Media Freedom (IMF), a five-year program implemented by Internews and funded by the U.S. Agency for International Development (USAID) with the support of the American people.">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://totooba.info/assets/img/Group%20837.png">
+    <meta property="og:image:secure_url" content="https://totooba.info/assets/img/Group%20837.png">
+    <meta property="og:image:width" content="720">
+    <meta property="og:image:height" content="508">
+    <meta property="og:image:alt" content="TotooBa.info">
+    <meta property="og:image:type" content="image/jpeg">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="TotooBa.info">
+    <meta name="twitter:description" content="TotooBa.Info is a disinformation reporting platform developed under the Initiative for Media Freedom (IMF), a five-year program implemented by Internews and funded by the U.S. Agency for International Development (USAID) with the support of the American people.">
+    <meta name="twitter:image" content="https://totooba.info/assets/img/Group%20837.png">
+
+
     <link rel="shortcut icon" href="<?php echo base_url('assets/images/logo/logo.jpg') ?>" />
-    <title>Official Genteel Home - Furniture + Boutique + Lifestyle</title>
+   
     <link rel="stylesheet" media="all" href="<?php echo base_url('assets_website/css/bundle.css'); ?>" />
     <link rel="stylesheet" media="all" href="<?php echo base_url('assets_website/css/style.css'); ?>" />
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets_website/custom-slide/css/demo.css'); ?>"/>
