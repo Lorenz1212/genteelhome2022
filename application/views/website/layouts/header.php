8670b58a10b851e15018cb4b1265ff5c368adf24
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-07-03 18:26:43 +0800

    Update header.php

--- application/views/website/layouts/header.php
+++ application/views/website/layouts/header.php
@@ -4,35 +4,24 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <!-- Mobile Web-app fullscreen -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
-    <meta name="description" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
-    <meta name="author" content="Genteel Home">
-
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+  
      <title>Official Genteel Home - Furniture + Boutique + Lifestyle</title>
-
     <meta property="og:title" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
     <meta property="og:site_name" content="TotooBa.info">
     <meta property="og:url" content="https://totooba.info/">
-    <meta property="og:description" content="TotooBa.Info is a disinformation reporting platform developed under the Initiative for Media Freedom (IMF), a five-year program implemented by Internews and funded by the U.S. Agency for International Development (USAID) with the support of the American people.">
+    <meta property="og:description" content="GenteelHome takes its designs to a premier level as its pieces bring style in your homes, your offices, your restaurants and the like. Even more so, Genteel sets itself apart in its designs for mirroring the characters of each of its clientele.">
     <meta property="og:type" content="website">
-    <meta property="og:image" content="https://totooba.info/assets/img/Group%20837.png">
-    <meta property="og:image:secure_url" content="https://totooba.info/assets/img/Group%20837.png">
+    <meta property="og:image" content="https://miracle-tree.org/genteelhomev2/assets/images/logo/logo2.jpg">
+    <meta property="og:image:secure_url" content="https://miracle-tree.org/genteelhomev2/assets/images/logo/logo2.jpg">
     <meta property="og:image:width" content="720">
     <meta property="og:image:height" content="508">
     <meta property="og:image:alt" content="TotooBa.info">
     <meta property="og:image:type" content="image/jpeg">
-
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="TotooBa.info">
-    <meta name="twitter:description" content="TotooBa.Info is a disinformation reporting platform developed under the Initiative for Media Freedom (IMF), a five-year program implemented by Internews and funded by the U.S. Agency for International Development (USAID) with the support of the American people.">
-    <meta name="twitter:image" content="https://totooba.info/assets/img/Group%20837.png">
-
-
     <link rel="shortcut icon" href="<?php echo base_url('assets/images/logo/logo.jpg') ?>" />
    
     <link rel="stylesheet" media="all" href="<?php echo base_url('assets_website/css/bundle.css'); ?>" />
