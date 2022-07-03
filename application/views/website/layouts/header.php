29e01e33cc673d5f15a9acd729ff4de0543861bd
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-07-03 21:30:52 +0800

    Update header.php

--- application/views/website/layouts/header.php
+++ application/views/website/layouts/header.php
@@ -8,7 +8,6 @@
     <!-- Mobile Web-app fullscreen -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="mobile-web-app-capable" content="yes">
-    <meta charset="utf-8">
   
     <title>Official Genteel Home - Furniture + Boutique + Lifestyle</title>
     <meta property="og:title" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
@@ -16,8 +15,8 @@
     <meta property="og:url" content="<?php echo base_url()?>">
     <meta property="og:description" content="GenteelHome takes its designs to a premier level as its pieces bring style in your homes, your offices, your restaurants and the like. Even more so, Genteel sets itself apart in its designs for mirroring the characters of each of its clientele.">
     <meta property="og:type" content="website">
-    <meta property="og:image" content="<?php echo base_url('assets/images/logo/logo2.JPG')?>">
-    <meta property="og:image:secure_url" content="<?php echo base_url('assets/images/logo/logo2.jpg')?>">
+    <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQAekUpHY_pAUZLhA9AT6K4Eh0-l0FO5vQnuwLHfAbnCD5N1yHGgiBMbOBXe8tkiI7wQ&usqp=CAU">
+    <meta property="og:image:secure_url" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQAekUpHY_pAUZLhA9AT6K4Eh0-l0FO5vQnuwLHfAbnCD5N1yHGgiBMbOBXe8tkiI7wQ&usqp=CAU">
     <meta property="og:image:width" content="400">
     <meta property="og:image:height" content="300">
     <meta property="og:image:alt" content="Genteelhome.ph">
@@ -26,7 +25,7 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
     <meta name="twitter:description" content="GenteelHome takes its designs to a premier level as its pieces bring style in your homes, your offices, your restaurants and the like. Even more so, Genteel sets itself apart in its designs for mirroring the characters of each of its clientele.">
-    <meta name="twitter:image" content="<?php echo base_url('assets/images/logo/logo2.JPG')?>">
+    <meta name="twitter:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsQAekUpHY_pAUZLhA9AT6K4Eh0-l0FO5vQnuwLHfAbnCD5N1yHGgiBMbOBXe8tkiI7wQ&usqp=CAU">
 
 
     <link rel="shortcut icon" href="<?php echo base_url('assets/images/logo/logo.jpg') ?>" />
