68f8eaf3abd28dd5bd8f99f6efc6607349a24974
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-07-03 18:30:00 +0800

    Update header.php

--- application/views/website/layouts/header.php
+++ application/views/website/layouts/header.php
@@ -20,8 +20,15 @@
     <meta property="og:image:secure_url" content="https://miracle-tree.org/genteelhomev2/assets/images/logo/logo2.jpg">
     <meta property="og:image:width" content="720">
     <meta property="og:image:height" content="508">
-    <meta property="og:image:alt" content="TotooBa.info">
+    <meta property="og:image:alt" content="Genteelhome.ph">
     <meta property="og:image:type" content="image/jpeg">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Official Genteel Home - Furniture + Boutique + Lifestyle">
+    <meta name="twitter:description" content="GenteelHome takes its designs to a premier level as its pieces bring style in your homes, your offices, your restaurants and the like. Even more so, Genteel sets itself apart in its designs for mirroring the characters of each of its clientele.">
+    <meta name="twitter:image" content="https://miracle-tree.org/genteelhomev2/assets/images/logo/logo2.jpg">
+
+
     <link rel="shortcut icon" href="<?php echo base_url('assets/images/logo/logo.jpg') ?>" />
    
     <link rel="stylesheet" media="all" href="<?php echo base_url('assets_website/css/bundle.css'); ?>" />
@@ -31,4 +38,17 @@
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets_website/css/slick-theme.css"/>
     <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/plugins/custom/prismjs/prismjs.bundle.css" rel="stylesheet" type="text/css" />
+    <script async="" src="//matomo.barangayhub.ph/matomo.js"></script><script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.barangayhub.ph/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
 </head>
