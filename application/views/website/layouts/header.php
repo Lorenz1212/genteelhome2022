d851e8470dfc9988485a3823aa1452ae25434e8a
Lorenz1212 <42542401+Lorenz1212@users.noreply.github.com>
2022-07-03 21:03:46 +0800

    Update header.php

--- application/views/website/layouts/header.php
+++ application/views/website/layouts/header.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <!-- Mobile Web-app fullscreen -->
     <meta name="apple-mobile-web-app-capable" content="yes">
